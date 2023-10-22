c6a1791eefeab68542a9e667c68adf18c13d33d6
Xavizard <xavi.figso@gmail.com>
2023-10-22 13:40:18 -0600

    Created DetailActivity and did design for the xml using constraint layout.

--- app/src/main/java/com/xavizard/registros/DetailActivity.kt
+++ app/src/main/java/com/xavizard/registros/DetailActivity.kt
@@ -2,12 +2,10 @@ package com.xavizard.registros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.xavizard.registros.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_detail)
     }
 }
