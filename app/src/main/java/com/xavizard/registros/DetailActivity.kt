98dd604c29b8aa5e53e1117b41b58c76c83b0ab0
Xavizard <xavi.figso@gmail.com>
2023-10-27 14:20:02 -0600

    Added Intent logic to pass info from MainActivity to DetailActivity. Working as expected

--- app/src/main/java/com/xavizard/registros/DetailActivity.kt
+++ app/src/main/java/com/xavizard/registros/DetailActivity.kt
@@ -2,10 +2,25 @@ package com.xavizard.registros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.xavizard.registros.databinding.ActivityDetailBinding
+import java.util.zip.Inflater
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras!!
+        val personName = bundle.getString("person_name")
+        val personCharge = bundle.getString("person_charge")
+        val chargeDescription = bundle.getString("charge_description")
+        val personRating = bundle.getFloat("person_rating")
+
+        binding.personName.text = personName
+        binding.personCharge.text = personCharge
+        binding.personDescription.text = chargeDescription
+        binding.ratingBar.rating = personRating
     }
 }
--- app/src/main/java/com/xavizard/registros/MainActivity.kt
+++ app/src/main/java/com/xavizard/registros/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.xavizard.registros
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.xavizard.registros.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener {
+            val personName = binding.nombreEdit.text.toString()
+            val personCharge = binding.cargoEdit.text.toString()
+            val chargeDescription = binding.descriptionEdit.text.toString()
+            val personRating = binding.rateBar.rating
+            openDetailActivity(personName, personCharge, chargeDescription, personRating) }
+
+    }
+
+    private fun openDetailActivity(personName: String, personCharge: String, chargeDescription: String, personRating: Float){
+        val intent = Intent(this,DetailActivity::class.java)
+        intent.putExtra("person_name", personName)
+        intent.putExtra("person_charge", personCharge)
+        intent.putExtra("charge_description", chargeDescription)
+        intent.putExtra("person_rating", personRating)
+        startActivity(intent)
     }
 }
