b252a554b05049355863cdaa48b138aea7879df4
Xavizard <xavi.figso@gmail.com>
2024-03-05 15:30:35 -0600

    Solved error: When tapping pic button, no more crashing Got error: When passing pic to next activity, crashes

--- app/src/main/java/com/xavizard/registros/MainActivity.kt
+++ app/src/main/java/com/xavizard/registros/MainActivity.kt
@@ -2,21 +2,31 @@ package com.xavizard.registros
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
 import com.xavizard.registros.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var profilePic: ImageView
     private var profileBitmap: Bitmap? = null
+    private var picPath = ""
 
-    val getContent = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){
-        bitmap ->
-        profileBitmap = bitmap
-        profilePic.setImageBitmap(profileBitmap!!)
+    private val getContent = registerForActivityResult(ActivityResultContracts.TakePicture()){
+        success ->
+        if(success && picPath.isNotEmpty()){
+            profileBitmap = BitmapFactory.decodeFile(picPath)
+            profilePic.setImageBitmap(profileBitmap)
+        }
+
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +51,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openCamera() {
-        getContent.launch(null)
+        val file = createImageFile()
+        val uri = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            FileProvider.getUriForFile(this,
+                "$packageName.provider",
+                file)
+        } else {
+            Uri.fromFile(file)
+        }
+        getContent.launch(uri)
+    }
+
+    private fun createImageFile():File {
+        val fileName = "profilePicImage"
+        val fileDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File.createTempFile(fileName, ".jpg", fileDirectory)
+        picPath =file.absolutePath
+        return file
     }
 
     private fun openDetailActivity(persona: Persona){
